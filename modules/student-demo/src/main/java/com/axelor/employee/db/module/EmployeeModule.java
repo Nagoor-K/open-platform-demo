326ec1d4c4eeaadf9d14ee83d10149ddaecce9f2
Nagoor-K <p.nagoor@axelor.com>
2022-03-31 19:22:31 +0530

    data import in aop

--- modules/student-demo/src/main/java/com/axelor/employee/db/module/EmployeeModule.java
+++ modules/student-demo/src/main/java/com/axelor/employee/db/module/EmployeeModule.java
@@ -3,11 +3,14 @@ package com.axelor.employee.db.module;
 import com.axelor.app.AxelorModule;
 import com.axelor.employee.db.service.EmployeeService;
 import com.axelor.employee.db.service.EmployeeServiceImpl;
+import com.axelor.student.db.Project;
+import com.axelor.student.db.repo.ProjectRepository;
 
 public class EmployeeModule extends AxelorModule{
 	
 	@Override
 	  protected void configure() {
 	   bind(EmployeeService.class).to(EmployeeServiceImpl.class);
+	   bind(ProjectRepository.class);
 	  }
 }
