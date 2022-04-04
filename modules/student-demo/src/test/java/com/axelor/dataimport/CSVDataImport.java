1ab02715c184c112c1a06ea6cd4ab795f7956ceb
Nagoor-K <p.nagoor@axelor.com>
2022-04-04 19:01:24 +0530

    widgets, themes and reports

--- modules/student-demo/src/test/java/com/axelor/dataimport/CSVDataImport.java
+++ modules/student-demo/src/test/java/com/axelor/dataimport/CSVDataImport.java
@@ -6,16 +6,17 @@ import org.junit.Test;
 
 import com.axelor.data.Importer;
 import com.axelor.data.csv.CSVImporter;
+import com.axelor.employee.db.module.EmployeeModule;
 import com.axelor.inject.Beans;
 import com.axelor.student.db.repo.ProjectRepository;
-
+import com.axelor.test.GuiceModules;
+@GuiceModules(EmployeeModule.class)
 public class CSVDataImport {
 	
 	@Test
 	public void deaulttest() throws IOException{
 		
 		Importer importer=new CSVImporter("data-init/input-config.xml","data-init/csv");
-		//importer.run();
-		System.err.println(Beans.get(ProjectRepository.class).all().fetch());
+		importer.run();
 	}
 }
